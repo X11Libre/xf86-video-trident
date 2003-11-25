03cfac30d2a93d40779725b97be0dfa3d3ba33f2
Kaleb Keithley <kaleb@freedesktop.org>
2003-11-25 19:28:43 +0000

    XFree86 4.3.99.16 Bring the tree up to date for the Cygwin folks

--- src/trident_regs.h
+++ src/trident_regs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 1992-2000 by Alan Hourihane, Wigan, England.
+ * Copyright 1992-2003 by Alan Hourihane, North Wales, UK.
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
@@ -21,7 +21,7 @@
  *
  * Author:  Alan Hourihane, alanh@fairlite.demon.co.uk
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/trident/trident_regs.h,v 1.26 2002/09/16 18:06:03 eich Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/trident/trident_regs.h,v 1.27 2003/09/05 22:07:29 alanh Exp $ */
 
 #define DEBUG 1
 
@@ -119,6 +119,7 @@
 #define BiosNewMode1 0x5a
 #define BiosNewMode2 0x5c
 #define BiosReg      0x5d
+#define DisplayEngCont 0xD1
 
 /* Graphics Engine for 9420/9430 */
 
